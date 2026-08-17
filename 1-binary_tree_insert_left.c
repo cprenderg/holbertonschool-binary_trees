f96f0b8a2624728e36086911c5931bc8283dfe45
Christopher Prendergast <14800@holbertonstudents.com>
2026-08-17 12:47:58 +1000

    Fixed style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - inserts a new node as the left child of another node
+ * binary_tree_insert_left - inserts a node as the left child of another node
  * @parent: pointer ot node to isnert the left child in
  * @value: value to store in the new node
  *
@@ -20,6 +20,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
-	parent->left = new_node;	
+	parent->left = new_node;
 	return (new_node);
 }
