11a91586027f24a625241a5410c8b5d5e994f719
Christopher Prendergast <14800@holbertonstudents.com>
2026-08-17 14:16:30 +1000

    Updated binary_trees.h and added 3-binary_tree delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,15 +10,15 @@ static void binary_tree_delete_right(binary_tree_t *tree);
 static void binary_tree_delete_left(binary_tree_t *tree)
 {
 	if (tree->left == NULL)
-        {
-                return;
-        }
+	{
+		return;
+	}
 	if (tree->left->right)
 	{
 		binary_tree_delete_right(tree->left);
-	}    
-        binary_tree_delete_left(tree->left);
-        free(tree->left);
+	}
+		binary_tree_delete_left(tree->left);
+		free(tree->left);
 }
 /**
  * binary_tree_delete_right - deletes the right side of a binary tree
