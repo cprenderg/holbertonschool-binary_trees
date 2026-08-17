1c7c248ca25813fe00405cbd4f9465883a9969b9
Christopher Prendergast <14800@holbertonstudents.com>
2026-08-17 15:36:33 +1000

    Fixed logic

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,9 +7,9 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->parent == NULL)
+	if (node->right == NULL && node->left == NULL)
 	{
-		return 0;
+		return 1;
 	}
-	return (1);
+	return (0);
 }
