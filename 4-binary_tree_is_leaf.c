100a0bd355e0db4dc81ebe0bf1687f39c9a3c44c
Christopher Prendergast <14800@holbertonstudents.com>
2026-08-17 15:41:14 +1000

    Added return 0 for node == NULL

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,9 +7,13 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	if (node == NULL)
+	{
+		return (0);
+	}
 	if (node->right == NULL && node->left == NULL)
 	{
-		return 1;
+		return (1);
 	}
 	return (0);
 }
